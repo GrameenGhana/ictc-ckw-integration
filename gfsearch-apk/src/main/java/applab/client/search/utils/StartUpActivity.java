2e5fe148e7b53fc2d0e916a05e021512318ee051
Seth Kwakwa <skwakwa@grameenfoundation.org>
2015-08-28 16:47:03 +0000

    Implementation of Login

--- gfsearch-apk/src/main/java/applab/client/search/utils/StartUpActivity.java
+++ gfsearch-apk/src/main/java/applab/client/search/utils/StartUpActivity.java
@@ -72,28 +72,13 @@ public class StartUpActivity extends Activity {
         login_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 if (userEdit.getText().toString().isEmpty() || passEdit.getText().toString().isEmpty()) {
-//                    error.setText("Username and Password can't be empty");
                     Toast.makeText(getBaseContext(), "Username and Password can't be empty", Toast.LENGTH_LONG).show();
-//                    error.setBackgroundColor(R.);
-//                    error.setVisibility(TextView.VISIBLE);
-
-                } else {
+               } else {
                     Intent intent = new Intent(StartUpActivity.this, DashboardActivity.class);
                     databaseHelper = new DatabaseHelper(getBaseContext());
-                    /**
-                     * saveFarmer(String firstName, String lastName, String nickname, String community, String village, String district,
-                     String region, String age, String gender, String maritalStatus, String numberOfChildren, String numberOfDependants,
-                     String education, String cluster, String farmID)
-                     */
-//helper.saveFarmer("Kofi","Adarkwa","kadar","Kumasi","Ada","da","region","age","M","Single","2","2","shs","2","8i8i8i9i");
 
-//                myFarmers.add(new Farmer(farmer.getString("fname"),farmer.getString("lName"),farmer.getString("nickname"),farmer.getString("community"),farmer.getString("village"),farmer.getString("district"),farmer.getString("region"),farmer.getString("age"),farmer.getString("gender"),farmer.getString("ms"),farmer.getString("noc"),farmer.getString("nod"),farmer.getString("edu"),farmer.getString("cluster"),farmer.getString("id")));
-
-//                if (databaseHelper.farmerCount() < 1) {
-
-                    getData(intent,"");
-//            }
-                    startActivity(intent);
+                    getData(intent,"us="+userEdit.getText()+"&pwd="+passEdit.getText(),"login","Login Please wait");
+//
                 }
             }
         });
@@ -114,7 +99,7 @@ public class StartUpActivity extends Activity {
     }
 
 
-    public void getData(final Intent intent, final String queryString,String msg ) {
+    public void getData(final Intent intent, final String queryString, final String type,String msg ) {
 
         // String url="http://sandbox-ictchallenge.cs80.force.com/getTest";
 
@@ -128,7 +113,7 @@ public class StartUpActivity extends Activity {
 
                 try {      /* TODO output your page here. You may use following sample code. */
                     String serverResponse = "";
-                    String url = IctcCkwIntegrationSync.ICTC_SERVER_URL + "action=login";
+                    String url = IctcCkwIntegrationSync.ICTC_SERVER_URL + "action="+type;
                     if(queryString.length()>0)
                         url+="&"+queryString;
 
@@ -152,7 +137,7 @@ public class StartUpActivity extends Activity {
 
 //                    System.out.println("Serrver Rsponse  : "+serverResponse);
 
-                    threadMsg(serverResponse,"");
+                    threadMsg(serverResponse,type);
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (Exception e) {
@@ -166,6 +151,7 @@ public class StartUpActivity extends Activity {
                     Message msgObj = handler.obtainMessage();
                     Bundle b = new Bundle();
                     b.putString("message", msg);
+                    b.putString("type", type);
                     msgObj.setData(b);
                     handler.sendMessage(msgObj);
                 }
@@ -180,127 +166,103 @@ public class StartUpActivity extends Activity {
                     public void handleMessage(Message msg) {
 
 
+                        String type  = msg.getData().getString("type");
                         String aResponse = msg.getData().getString("message");
 
-                        if ((null != aResponse)) {
-
-                            ///  TextView test = (TextView) findViewById(R.id.txtcluster);
-
-                            // test.setText(aResponse);
+                        if(type.equalsIgnoreCase("login")){
 
                             try {
-                                JSONObject resp = new JSONObject(aResponse);
+                                startActivity(intent);
+                                if ((null != aResponse)) {
+                                    JSONObject resp = new JSONObject(aResponse);
+                                    if(resp.getString("rc").equalsIgnoreCase("00")){
+                                        Intent intent1 = new Intent(getBaseContext(),DashboardActivity.class);
+                                        Toast.makeText(getBaseContext(),"Login Successful",Toast.LENGTH_LONG).show();
+                                        getData(intent, "", "details","Login Successful, Loading Agent Data");
+                                    }else{
+                                        Toast.makeText(getBaseContext(),"Wrong Username or password",Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                                }catch(Exception e){
 
-                                JSONArray farmers = (JSONArray) resp.get("farmer");
+                            }
 
-                                databaseHelper.resetFarmer();
-                                int farmersCnt = 0;
-                                for (int i = 0; i < farmers.length(); i++) {
-                                    JSONObject farmer = farmers.getJSONObject(i);
+                        }else if(type.equalsIgnoreCase("details")){
+                            if ((null != aResponse)) {
+
+                                ///  TextView test = (TextView) findViewById(R.id.txtcluster);
+
+                                // test.setText(aResponse);
+
+                                try {
+                                    JSONObject resp = new JSONObject(aResponse);
+
+                                    JSONArray farmers = (JSONArray) resp.get("farmer");
+
+                                    databaseHelper.resetFarmer();
+                                    int farmersCnt = 0;
+                                    for (int i = 0; i < farmers.length(); i++) {
+                                        JSONObject farmer = farmers.getJSONObject(i);
 //                                    System.out.println("Farmer Name  : " + farmer.getString("lname"));
 //                                    cluster1List.add((String) j.get("farmer"));
 //
-                                    //   String []  keys =  {"fname","lname","nickname","community","village","district","ms","age","gender","ms","noc","nod","edu","cluster","id","maincrop"};
-                                    String[] keys
-                                            = {
+                                        //   String []  keys =  {"fname","lname","nickname","community","village","district","ms","age","gender","ms","noc","nod","edu","cluster","id","maincrop"};
+                                        String[] keys
+                                                = {
 
-                                            "fname", "lname", "nickname", "communy", "village", "district", "region", "age", "gender", "ms", "noc", "nod", "edu", "cluster", "id",
-                                            "sizeplot", "labour", "date_land_ident", "loc_land", "target_area",
-                                            "exp_price_ton", "variety", "target_nxt_season", "techneed1", "techneed2",
-                                            "fbo",
-                                            "landarea", "date_plant", "date_man_weed", "pos_contact", "mon_sell_start", "mon_fin_pro_sold",
-                                            "maincrop"
-                                    };
-                                    String[] vals = new String[keys.length];
-                                    int cnt = 0;
-                                    for (String key : keys) {
-                                        try {
-                                            vals[cnt] = farmer.getString(key);
-                                        } catch (Exception e) {
-                                            vals[cnt] = "";
+                                                "fname", "lname", "nickname", "communy", "village", "district", "region", "age", "gender", "ms", "noc", "nod", "edu", "cluster", "id",
+                                                "sizeplot", "labour", "date_land_ident", "loc_land", "target_area",
+                                                "exp_price_ton", "variety", "target_nxt_season", "techneed1", "techneed2",
+                                                "fbo",
+                                                "landarea", "date_plant", "date_man_weed", "pos_contact", "mon_sell_start", "mon_fin_pro_sold",
+                                                "maincrop"
+                                        };
+                                        String[] vals = new String[keys.length];
+                                        int cnt = 0;
+                                        for (String key : keys) {
+                                            try {
+                                                vals[cnt] = farmer.getString(key);
+                                            } catch (Exception e) {
+                                                vals[cnt] = "";
+                                            }
+                                            cnt++;
                                         }
-                                        cnt++;
+
+
+
+                                        databaseHelper.saveFarmer(vals[0], vals[1], vals[2], vals[3],
+                                                vals[4], vals[5], vals[6], vals[7], vals[8], vals[9], vals[10], vals[11], vals[12], vals[13], vals[14],
+                                                vals[15], vals[16], vals[17], vals[18],
+                                                vals[19], vals[20], vals[21], vals[22], vals[23], vals[24], vals[25], vals[26], vals[27], vals[28], vals[29], vals[30], vals[31], vals[32]);
+
+//                                    farmersCnt++;
                                     }
 
 
-                                    /**
-                                     *
-                                     String firstName, String lastName, String nickname, String community, String village, String district,
-                                     String region, String age, String gender, String maritalStatus, String numberOfChildren, String numberOfDependants,
-                                     String education, String cluster, String farmID,
-                                     String SIZE_PLOT,
-                                     String LABOUR,
-                                     String DATE_OF_LAND_IDENTIFICATION ,
-                                     String LOCATION_LAND,
-                                     String TARGET_AREA,
-                                     String EXPECTED_PRICE_TON ,
-                                     String VARIETY,
-                                     String TARGET_NEXT_SEASON ,
-                                     String TECH_NEEDS_I,
-                                     String TECH_NEEDS_II ,
-                                     String FARMER_BASE_ORG,
 
-                                     String PLANTING_DATE,
-                                     String LAND_AREA ,
-                                     String DATE_MANUAL_WEEDING ,
-                                     String POS_CONTACT ,
-                                     String MONTH_SELLING_STARTS ,
-                                     String MONTH_FINAL_PRODUCT_SOLD
 
-                                     */
-
-                                    databaseHelper.saveFarmer(vals[0], vals[1], vals[2], vals[3],
-                                            vals[4], vals[5], vals[6], vals[7], vals[8], vals[9], vals[10], vals[11], vals[12], vals[13], vals[14],
-                                            vals[15], vals[16], vals[17], vals[18],
-                                            vals[19], vals[20], vals[21], vals[22], vals[23], vals[24], vals[25], vals[26], vals[27], vals[28], vals[29], vals[30], vals[31], vals[32]);
-
-//                                    farmersCnt++;
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
                                 }
 
+                                // ALERT MESSAGE
+                                Toast.makeText(
+                                        getBaseContext(),
+                                        "Data Recieved: ",
 
-                                //sending data into cluster 1
-                                /**  Intent cluster1Intent = new Intent(MainActivity.this, Cluster1.class);
-                                 Bundle b = new Bundle();
-                                 b.putStringArrayList("cluster1", (ArrayList<String>) cluster1List);
-                                 cluster1Intent.putExtra("clusterbundle", b);
-                                 startActivity(cluster1Intent);
+                                        Toast.LENGTH_SHORT).show();
+                                startActivity(intent);
+                            } else {
 
-                                 //sending data into cluster 2
-                                 Intent cluster2Intent = new Intent(MainActivity.this, Cluster2.class);
-                                 Bundle b2 = new Bundle();
-                                 b2.putStringArrayList("cluster2", (ArrayList<String>) cluster1List);
-                                 cluster2Intent.putExtra("clusterbundle",b2);
-                                 startActivity(cluster2Intent);
-
-
-                                 //sending data into cluster 3
-                                 Intent cluster3Intent = new Intent(MainActivity.this, Cluster3.class);
-                                 Bundle b3 = new Bundle();
-                                 b3.putStringArrayList("cluster2", (ArrayList<String>) cluster1List);
-                                 cluster3Intent.putExtra("clusterbundle",b3);
-                                 startActivity(cluster3Intent);
-                                 **/
-
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+                                //ALERT MESSAGE
+                                Toast.makeText(
+                                        getBaseContext(),
+                                        "Not Got Response From Server.",
+                                        Toast.LENGTH_SHORT).show();
                             }
 
-                            // ALERT MESSAGE
-                            Toast.makeText(
-                                    getBaseContext(),
-                                    "Data Recieved: ",
-
-                                    Toast.LENGTH_SHORT).show();
-                            startActivity(intent);
-                        } else {
-
-                            //ALERT MESSAGE
-                            Toast.makeText(
-                                    getBaseContext(),
-                                    "Not Got Response From Server.",
-                                    Toast.LENGTH_SHORT).show();
                         }
+
 
                     }
                 };
