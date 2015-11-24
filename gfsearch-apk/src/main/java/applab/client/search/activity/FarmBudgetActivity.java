641e16affccc76d52892fccae62cf6ddf27f2ffb
Seth Kwakwa <skwakwa@grameenfoundation.org>
2015-11-24 10:06:33 +0000

    Change font size , included tab for farm Budget

---
+++ gfsearch-apk/src/main/java/applab/client/search/activity/FarmBudgetActivity.java
@@ -0,0 +1,14 @@
+package applab.client.search.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by skwakwa on 11/20/15.
+ */
+public class FarmBudgetActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
